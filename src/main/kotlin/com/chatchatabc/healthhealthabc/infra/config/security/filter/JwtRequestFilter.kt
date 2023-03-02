bc96ab618a00048bb6e6a36bde33a3263d94dd42
Raphile14 <rllddalangin@gmail.com>
2023-03-02 13:39:40 +0800

    feat: implemented logging and storing them to .log files

--- src/main/kotlin/com/chatchatabc/healthhealthabc/infra/config/security/filter/JwtRequestFilter.kt
+++ src/main/kotlin/com/chatchatabc/healthhealthabc/infra/config/security/filter/JwtRequestFilter.kt
@@ -6,18 +6,21 @@ import com.chatchatabc.healthhealthabc.domain.service.JwtService
 import jakarta.servlet.FilterChain
 import jakarta.servlet.http.HttpServletRequest
 import jakarta.servlet.http.HttpServletResponse
+import org.slf4j.LoggerFactory
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
 import org.springframework.security.core.context.SecurityContextHolder
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource
 import org.springframework.stereotype.Component
 import org.springframework.web.filter.OncePerRequestFilter
-import java.util.Optional
+import java.util.*
 
 @Component
 class JwtRequestFilter(
     private val jwtService: JwtService,
     private val userRepository: UserRepository
 ) : OncePerRequestFilter() {
+
+    private val log = LoggerFactory.getLogger(JwtRequestFilter::class.java)
 
     /**
      * Filter the request and validate the token
@@ -27,6 +30,9 @@ class JwtRequestFilter(
         response: HttpServletResponse,
         filterChain: FilterChain
     ) {
+        // Log the path of the request
+        log.info("Request path: ${request.method} ${request.requestURL} from ${request.remoteAddr}")
+
         // Get X-Access-Token from the request header
         val header: String? = request.getHeader("X-Access-Token")
 
