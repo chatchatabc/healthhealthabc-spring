db922b507b63a42da84f748fcd986356efb97842
Raphile14 <rllddalangin@gmail.com>
2023-03-13 14:50:30 +0800

    chore: deleted unnecessary dto files from provider

--- healthhealthabc-service/src/main/kotlin/com/chatchatabc/healthhealthabc/application/dto/auth/LoginRequest.kt
+++
@@ -1,4 +0,0 @@
-package com.chatchatabc.healthhealthabc.application.dto.auth
-
-data class LoginRequest(var username: String, var password: String) {
-}
--- healthhealthabc-service/src/main/kotlin/com/chatchatabc/healthhealthabc/application/dto/auth/RegisterResponse.kt
+++
@@ -1,7 +0,0 @@
-package com.chatchatabc.healthhealthabc.application.dto.auth
-
-import com.chatchatabc.api.application.dto.user.UserDTO
-import com.chatchatabc.healthhealthabc.application.dto.ErrorContent
-
-data class RegisterResponse(var user: UserDTO?, var errorContent: ErrorContent?) {
-}
