cce90411d90c00ed3ff21060e153404e3daafba5
Raphile14 <rllddalangin@gmail.com>
2023-03-14 10:58:11 +0800

    chore: removed unnecessary dtos from provider

--- healthhealthabc-service/src/main/kotlin/com/chatchatabc/healthhealthabc/application/dto/auth/ForgotPasswordResponse.kt
+++
@@ -1,6 +0,0 @@
-package com.chatchatabc.healthhealthabc.application.dto.auth
-
-import com.chatchatabc.healthhealthabc.application.dto.ErrorContent
-
-data class ForgotPasswordResponse (var errorContent: ErrorContent?) {
-}
--- healthhealthabc-service/src/main/kotlin/com/chatchatabc/healthhealthabc/application/dto/auth/ResetPasswordRequest.kt
+++
@@ -1,4 +0,0 @@
-package com.chatchatabc.healthhealthabc.application.dto.auth
-
-data class ResetPasswordRequest(val email: String, val password: String, val recoveryCode: String) {
-}
