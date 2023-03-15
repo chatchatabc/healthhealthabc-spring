dafef38e80a19a544816e93793ad50da32f12629
Raphile14 <rllddalangin@gmail.com>
2023-03-15 12:02:15 +0800

    chore: added implementation to serializable for UserProfileResponse.java

--- healthhealthabc-api/src/main/java/com/chatchatabc/api/application/dto/user/UserProfileResponse.java
+++ healthhealthabc-api/src/main/java/com/chatchatabc/api/application/dto/user/UserProfileResponse.java
@@ -5,10 +5,12 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.io.Serializable;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class UserProfileResponse {
+public class UserProfileResponse implements Serializable {
     private UserDTO user;
     private ErrorContent errorContent;
 }
